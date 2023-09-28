5a3b7201d2e78d68d2b1049b5f9af4a1783809b4
yunustalhac <yunustalhacoban@gmail.com>
2023-09-28 13:56:14 +0300

    bismillah

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import {createApp} from 'vue'
 import "@/assets/assets.css"
 import App from './App.vue'
-import { appAxios } from "@/utils/appAxios";
 
 
 
 
 const app = createApp(App)
-app.provide("appAxios", appAxios);
 app.mount('#app')
